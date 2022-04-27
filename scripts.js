7a974e81b0f02793a737ab4d0b96be38e9376f7e
Gustavo Camargos <gustavomcamargoss@gmail.com>
2022-04-26 22:27:22 -0300

    Create game() function

--- scripts.js
+++ scripts.js
@@ -1,34 +1,64 @@
 let moves = ["Rock", "Paper", "Scissor"];
 
 function computerPlay() {
+    
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection.toUpperCase() != "ROCK" && 
         playerSelection.toUpperCase() != "PAPER" &&
         playerSelection.toUpperCase() != "SCISSOR")  
     {
-        return `Text a valid option!`
+        return "-1"
     }
     else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        return `Tie Game! ${playerSelection.toUpperCase()} tie to ${computerSelection.toUpperCase()}`
+        return "0"
     }
     else if ((playerSelection.toUpperCase() == 'ROCK' && computerSelection.toUpperCase() == "SCISSOR") ||
     (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase() == "ROCK") ||
     (playerSelection.toUpperCase() == 'SCISSOR' && computerSelection.toUpperCase() == "PAPER"))
     {
-        return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        return "1"
     }
     else {
-        return `You lost! ${playerSelection.toUpperCase()} loses to ${computerSelection.toUpperCase()}`
+        return "2"
     }
 }
 
-const playerSelection = "scissor";
+function game() {
 
-const computerSelection = computerPlay();
+    let win = 0;
+    let lost = 0;
+    let tie = 0;
 
-const roundResult = playRound(playerSelection, computerSelection)
+    for(i=0; i<5; i++) {
+        let playerSelection = prompt("What's your move?");
+        const computerSelection = computerPlay();
+        const roundResult = playRound(playerSelection, computerSelection);
+        
+        switch(roundResult) {
+            case "-1":
+                console.log(`Text a valid option!`);
+                i -= 1;
+                break;
+            case "0":
+                console.log(`Tie Game! ${playerSelection.toUpperCase()} tie to ${computerSelection.toUpperCase()}`);
+                tie += 1;
+                break;
+            case "1":
+                console.log(`You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`);
+                win += 1;
+                break;
+            case "2":
+                console.log(`You lost! ${playerSelection.toUpperCase()} loses to ${computerSelection.toUpperCase()}`);
+                lost += 1;
+                break;
+        }
+    }
 
-console.log(roundResult);
+    console.log(`The scoreboard is:  ${win} wins, ${tie} ties and ${lost} loses.`)
+}
+
+game();
